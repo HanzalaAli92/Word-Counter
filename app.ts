4b5576ba4dd89921083305565597e60e42d236dd
HanzalaAli92 <hanzalaali20@gmail.com>
2024-04-17 23:32:58 +0500

    created file

---
+++ app.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+   //Import the 'inquirer' module, which is a command line interface for Node.js
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+  //Print the welcome message to the console
+console.log(chalk.bgGreenBright("Welcome to the Word Counter App"));
+
+  //Declare a let 'answers' and assign it to the result of inquirer.prompt function
+
+let answers: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: (chalk.bgBlueBright("Enter your sentence to count the word: "))
+    }
+])
+let words = answers.Sentence.trim().split(" ");
+
+ //Print the array of words to the console
+console.log(words)
+
+  //Print the words count of the sentence to the console
+console.log(chalk.bgRed(`Your sentence word count is ${words.length}`));
+
